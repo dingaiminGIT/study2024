4da2b9afd3891fa00c51a77aa73a805a3ba8fa60
aiminding <aiminding@tencent.com>
2024-02-06 15:03:41 +0800

    upate

--- src/main/java/com/baiya/study2024/spring/AService.java
+++ src/main/java/com/baiya/study2024/spring/AService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
  * springboot 2.6版本开始默认取消循环依赖的支持
  * https://www.springcloud.io/post/2022-02/spring-cyclic-dependencies/#gsc.tab=0
  * 可以用 spring.main.allow-circular-references=true 解决
+ *
  */
 @Service
 public class AService {
